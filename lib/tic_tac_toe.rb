f9600039548c282062e7dec8743c7e559071f4f2
Tim <timtran007@gmail.com>
2023-01-03 21:24:58 -0500

    commit tree a0f1fd31e3d8b644ef6c918ca9fa4bf5d872b785

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,10 @@
+class TicTacToe
+    
+    def initialize
+        @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]   
+    end
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[3]}"
+    end
+end
